284c91dd2a06e255d1c6d6a782bec4589a5159ea
Nerimiah Jay Ato <ndato@student.apc.edu.ph>
2024-12-16 22:57:49 +0800

    html dom

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,3 +1,8 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+function button1() {
+  document.getElementById("paragraph").innerHTML = "Paragraph changed.";
+}
+  
+function button2() {
+  document.getElementById("color").style.color = #2d6a4f;
+  document.getElementById("color").innerHTML = "Green!"
+}
