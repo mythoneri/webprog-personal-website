30a689ea121ab840b751072f372446f64cc9966b
Nerimiah Jay Ato <ndato@student.apc.edu.ph>
2024-12-16 23:20:26 +0800

    changes

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,8 +1,8 @@
-function Function1() {
-  document.getElementById("paragraph").innerHTML = "Paragraph changed.";
+function b1() {
+  document.getElementById("paragraph").innerHTML = "“When you repeat a mistake, it is not a mistake anymore: it is a decision.” - Paulo Coelho";
 }
   
-function Function2() {
-  document.getElementById("color").style.color = #344e41;
-  document.getElementById("color").innerHTML = "Green!"
+function b2() {
+  document.getElementById("color").style.color = "green";
+  document.getElementById("color").innerHTML = "My favorite color is Green!"
 }
